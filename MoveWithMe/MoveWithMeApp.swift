72e9b0c7c2c203b8a6e260114130831b737a8d7f
zhengxingXue <zx252cornell@gmail.com>
2021-10-28 20:09:04 -0400

    Initial Commit

---
+++ MoveWithMe/MoveWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveWithMeApp.swift
+//  MoveWithMe
+//
+//  Created by Jim's MacBook Pro on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MoveWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
