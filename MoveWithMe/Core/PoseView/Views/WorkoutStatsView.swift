a8f4b13ef524cbc4ef4d7743fbcce264e722e98c
zhengxingXue <zx252cornell@gmail.com>
2021-11-12 16:12:10 -0500

    change ipad stats view to tabview

--- MoveWithMe/Core/PoseView/Views/WorkoutStatsView.swift
+++ MoveWithMe/Core/PoseView/Views/WorkoutStatsView.swift
@@ -13,60 +13,10 @@ struct WorkoutStatsView: View {
     
     var body: some View {
         if isiPad {
-            iPadStatsView
+            iPadTabView
         } else {
             EmptyView()
         }
-    }
-    
-    private var iPadStatsView: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.black.opacity(0.6))
-            VStack {
-                HStack(spacing: 10) {
-                    VStack(spacing: 20) {
-                        makeCircleIndicator(angle: poseVM.rightArmAngel)
-                        makeCircleIndicator(angle: poseVM.rightLegAngle)
-                    }
-                    
-                    Image(systemName: "figure.walk")
-                        .resizable()
-                        .scaledToFit()
-                    
-                    VStack(spacing: 20) {
-                        makeCircleIndicator(angle: poseVM.leftArmAngle)
-                        makeCircleIndicator(angle: poseVM.leftLegAngel)
-                    }
-                }
-                Spacer()
-//                HStack {
-//                    Spacer()
-//                    Button {
-//                        print("DEBUG: Collapse stats section")
-//                    } label: {
-//                        Image(systemName: "chevron.left")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 10)
-//                    }
-//                }
-            }
-            .foregroundColor(.white)
-            .padding()
-        }
-        .frame(width: 300, height: 200)
-    }
-    
-    @ViewBuilder
-    private func makeCircleIndicator(angle: Angle?, showData: Bool = false) -> some View {
-        CircleIndicator(
-            data: (angle?.degrees ?? 90),
-            showData: showData,
-            color: .orange,
-            lineWidth: 10,
-            trimRatio: (angle?.degrees ?? 90) / 180
-        )
     }
 }
 
@@ -77,4 +27,50 @@ struct WorkoutStatsView_Previews: PreviewProvider {
             .previewDevice("iPad Pro (11-inch) (3rd generation)")
             .previewInterfaceOrientation(.landscapeLeft)
     }
+}
+
+extension WorkoutStatsView {
+    
+    private var iPadTabView: some View {
+        TabView {
+            circleIndicatorWithFigureView
+        }
+        .foregroundColor(.white)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.black.opacity(0.6))
+        )
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(width: 300, height: 200)
+    }
+    
+    private var circleIndicatorWithFigureView: some View {
+        HStack(spacing: 10) {
+            VStack(spacing: 20) {
+                makeCircleIndicator(angle: poseVM.rightArmAngel)
+                makeCircleIndicator(angle: poseVM.rightLegAngle)
+            }
+            
+            Image(systemName: "figure.walk")
+                .resizable()
+                .scaledToFit()
+            
+            VStack(spacing: 20) {
+                makeCircleIndicator(angle: poseVM.leftArmAngle)
+                makeCircleIndicator(angle: poseVM.leftLegAngel)
+            }
+        }
+        .padding()
+    }
+    
+    @ViewBuilder private func makeCircleIndicator(angle: Angle?, showData: Bool = false) -> some View {
+        CircleIndicator(
+            data: (angle?.degrees ?? 90),
+            showData: showData,
+            color: .orange,
+            lineWidth: 10,
+            trimRatio: (angle?.degrees ?? 90) / 180
+        )
+    }
+    
 }
