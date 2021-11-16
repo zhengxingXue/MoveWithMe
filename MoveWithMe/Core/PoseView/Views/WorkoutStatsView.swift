25f142683fddb678c7b7d2d78b5f0106d36ed096
zhengxingXue <zx252cornell@gmail.com>
2021-11-16 12:36:41 -0500

    jumping jack count (support both iphone and ipad)

--- MoveWithMe/Core/PoseView/Views/WorkoutStatsView.swift
+++ MoveWithMe/Core/PoseView/Views/WorkoutStatsView.swift
@@ -14,6 +14,8 @@ struct WorkoutStatsView: View {
     var body: some View {
         if isiPad {
             iPadTabView
+        } else if isiPhone {
+            iPhoneTabView
         } else {
             EmptyView()
         }
@@ -22,19 +24,22 @@ struct WorkoutStatsView: View {
 
 struct WorkoutStatsView_Previews: PreviewProvider {
     static var previews: some View {
+//        PoseView()
+//            .preferredColorScheme(.light)
+//            .previewDevice("iPad Pro (11-inch) (3rd generation)")
+//            .previewInterfaceOrientation(.landscapeLeft)
         PoseView()
-            .preferredColorScheme(.light)
-            .previewDevice("iPad Pro (11-inch) (3rd generation)")
-            .previewInterfaceOrientation(.landscapeLeft)
+            .previewDevice("iPhone 11 Pro Max")
     }
 }
 
 extension WorkoutStatsView {
     
+    // MARK: iPad
     private var iPadTabView: some View {
         TabView {
-            repCountView
-            circleIndicatorWithFigureView
+            iPadRepCountView
+            iPadCircleIndicatorWithFigureView
         }
         .foregroundColor(.white)
         .background(
@@ -45,7 +50,7 @@ extension WorkoutStatsView {
         .frame(width: 300, height: 200)
     }
     
-    private var repCountView: some View {
+    private var iPadRepCountView: some View {
         HStack(spacing: 0) {
             makeRepCountIndicator(for: poseVM.exercise)
                 .padding(30)
@@ -53,17 +58,7 @@ extension WorkoutStatsView {
         }
     }
     
-    @ViewBuilder private func makeRepCountIndicator(for exercise: Exercise, color: Color = .orange, lineWidth: CGFloat = 16) -> some View {
-        CircleIndicator(
-            data: exercise.repCount,
-            upperBound: exercise.repetition,
-            showData: true,
-            color: color,
-            lineWidth: lineWidth
-        )
-    }
-    
-    private var circleIndicatorWithFigureView: some View {
+    private var iPadCircleIndicatorWithFigureView: some View {
         HStack(spacing: 10) {
             VStack(spacing: 20) {
                 makeCircleIndicator(angle: poseVM.rightArmAngel)
@@ -82,13 +77,61 @@ extension WorkoutStatsView {
         .padding()
     }
     
-    @ViewBuilder private func makeCircleIndicator(angle: Angle?, showData: Bool = false) -> some View {
+    // MARK: iPhone
+    private var iPhoneTabView: some View {
+        TabView {
+            iPhoneRepCountView
+            iPhoneCircleIndicatorWithFigureView
+        }
+        .foregroundColor(.white)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.black.opacity(0.6))
+        )
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(width: 150, height: 150)
+    }
+    
+    private var iPhoneRepCountView: some View {
+        HStack(spacing: 0) {
+            makeRepCountIndicator(for: poseVM.exercise, lineWidth: 10)
+                .padding(30)
+                .font(.system(size: 40))
+        }
+    }
+    
+    private var iPhoneCircleIndicatorWithFigureView: some View {
+        HStack(spacing: 10) {
+            VStack(spacing: 20) {
+                makeCircleIndicator(angle: poseVM.rightArmAngel, lineWidth: 6)
+                makeCircleIndicator(angle: poseVM.rightLegAngle, lineWidth: 6)
+            }
+            VStack(spacing: 20) {
+                makeCircleIndicator(angle: poseVM.leftArmAngle, lineWidth: 6)
+                makeCircleIndicator(angle: poseVM.leftLegAngel, lineWidth: 6)
+            }
+        }
+        .padding()
+    }
+    
+    // MARK: View Builder
+    @ViewBuilder private func makeRepCountIndicator(for exercise: Exercise, color: Color = .orange, lineWidth: CGFloat = 16) -> some View {
+        CircleIndicator(
+            data: exercise.repCount,
+            upperBound: exercise.repetition,
+            showData: true,
+            color: color,
+            lineWidth: lineWidth
+        )
+    }
+    
+    @ViewBuilder private func makeCircleIndicator(angle: Angle?, showData: Bool = false, lineWidth: CGFloat = 10) -> some View {
         CircleIndicator(
             data: (angle?.degrees ?? 90),
             upperBound: 180,
             showData: showData,
             color: .orange,
-            lineWidth: 10
+            lineWidth: lineWidth
         )
     }
     
